6f41756f719ccbdd21e9ebb2085a88e3d4b98479
irhadLjubcic1 <irhad@gmail.com>
2019-03-24 16:01:42 +0100

    tst3
    asdsad

--- client/src/util/apiCaller.js
+++ client/src/util/apiCaller.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-export const API_URL = (process.env.BASE_URL || 'http://localhost:3000') + '/api';
+export const API_URL = window.location.hostname + '/api';
 
 
 export default function callApi(endpoint, method = 'get', body) {
